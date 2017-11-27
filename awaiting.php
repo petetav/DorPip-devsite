971740c16a6394e0539936144aa2f934d30b00bf
petetav <petethepilot69@gmail.com>
2017-11-27 14:12:36 +0000

    initial commit

---
+++ awaiting.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>	
+	<title> </title>
+	<meta name=“description” content= “”>
+	
+ <?php include 'includes/head.php'; ?> 
+</head>
+
+<body>
+		<div class="static_top">
+<!--masthead -->
+	<?php include 'includes/masthead.php'; ?>		
+<div class="bg_gradient" width="100%"> 
+<!--navbar-->		
+	<?php include 'includes/navbar-1.php'; ?> 
+</div>
+</div> <!--end of Static_top -->
+
+<div class="container-noflex"> <!-- or use no-flex if required -->
+<div class="blurb-noflex"> <!-- or use no-flex if required -->
+	
+<div class="awaiting-content">
+	<p>When we've got some content this page will look amazing!</p> 	
+</div>
+	
+	
+	
+	
+</div> <!-- end of blurb -->
+</div> <!--end of container-->
+
+<footer>
+	<?php include 'includes/footer.php'; ?>
+</footer>
+
+</body>
+</html>	
---
+++ events.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>	
+	<title>Dorpip content="Dorset Parent infant partnership</title>
+	<meta name=“description” content= “Find out all about the Dorset Parent Infant Partnership.”>
+	<?php include 'includes/head.php'; ?>
+</head>
+
+
+<body>
+		<div class="static_top">
+	<?php include 'includes/masthead.php'; ?>
+	
+<div class="bg_gradient" width="100%">
+		
+	<?php include 'includes/navbar-1.php'; ?> 
+</div>
+</div> <!--end of Static_top -->
+
+<div class="container">
+
+<div class="blurb">
+<ul>
+<li>Launch Event at Bournemouth University  (use their logo) Date TBA</li>
+
+
+<li>Gala Ball 2018 (any image of a fancy do)</li>
+
+<li>Sponsored events</li>
+
+<li>Run Walk Swim Cycle</li>
+
+<li>ENQUIRE NOW if you’d like to organise an event or take part in one.</li>
+
+<li>Legacy Pledges<li>
+
+
+<h3><a href="https://www.youtube.com/watch?v=5rvaYoaBsmA"> Watch this short film</a></h3>
+
+
+<h4>Other ways to donate</h4>
+
+<p>Think about leaving a gift for Dorset Parent Infant Partnership in your Will and help make a happy, healthy childhood and a fulfilling life one baby at a time.</p> 
+
+<p>Enquire Now for a legacy pack</p>
+</div> <!-- end of blurb -->
+</div> <!-- end of container --> 
+<footer>
+	<?php include 'includes/footer.php'; ?>
+</footer>
+
+</body>
+</html>
