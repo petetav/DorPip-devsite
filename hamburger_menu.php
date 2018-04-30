4042614237b2ae273530055b1e48a90824de8a52
petetav <petethepilot69@gmail.com>
2018-04-30 07:17:42 +0100

    almost mobile friendly

---
+++ hamburger_menu.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>
+	
+	<title>Home page </title>
+	<meta name="description" content="All about the Dorset Parent Infant Partnership.">
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Pete Tavener">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/custom_variables.css">	
+	<link rel="stylesheet" href="css/hamburger_menu.css">
+	
+	<link rel="shortcut icon" href="/img/icons/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/img/icons/favicon.ico" type="image/x-icon">
+	
+
+
+</head>
+
+<body>
+	<div class="static_top">
+<!-- masthead -->
+	<?php include 'includes/masthead.php'; ?>	
+<!-- <div class="bg_gradient" width="100%"> -->
+
+
+</div> <!-- end of Static_top -->
+
+
+
+<div class="banner-photos-container"> 
+	
+	<div class="banner-photo">
+		<img src="img/photos/small/mother-daughter-love-sunset.jpg" alt="mother-daughter-love-sunset"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/baby_on_shoulder.jpg" alt="baby_on_shoulder" width="100%"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/baby_hand.jpg" alt="baby_hand" width="100%"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/mother-baby%20cuddle.jpg" alt="mother-baby%20cuddle"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/father_%26_baby_sunset.jpg" alt="father_%26_baby_sunset"></div>
+	
+</div>
+
+	
+
+
+<div class="container-noflex">
+
+<div class="blurb-noflex">
+	
+<!--page stuff goes here -->
+<!-- css vertical menu and submenu from codepen -->
+<?php include 'includes/navbar-1.php'; ?>
+
+
+<br/><br/><br/>
+
+</div> <!-- end of blurb -->
+	
+
+</div> <!-- end of container -->
+<br><br>
+<footer>
+	<?php include 'includes/footer.php'; ?>
+</footer>
+
+</body>
+</html>
