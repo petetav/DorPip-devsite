33882e2c4b41a7e38612f1169e394adfd7e5d048
petetav <petethepilot69@gmail.com>
2018-05-01 17:32:12 +0100

    Logo changed and testimonials added

---
+++ testimonials.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<head>
+	
+	<title>Home page </title>
+	<meta name="description" content="All about the Dorset Parent Infant Partnership.">
+	
+	<?php include 'includes/head.php'; ?>	
+	
+
+</head>
+
+<body>
+	<div class="static_top">
+<!-- masthead -->
+	<?php include 'includes/masthead.php'; ?>	
+<!-- <div class="bg_gradient" width="100%"> -->
+
+<!-- navbar -->
+	<?php include 'includes/navbar-1.php'; ?> 
+
+</div> <!-- end of Static_top -->
+
+
+
+<div class="banner-photos-container"> 
+	
+	<div class="banner-photo">
+		<img src="img/photos/small/mother-daughter-love-sunset.jpg" alt="mother-daughter-love-sunset"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/baby_on_shoulder.jpg" alt="baby_on_shoulder" width="100%"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/baby_hand.jpg" alt="baby_hand" width="100%"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/mother-baby%20cuddle.jpg" alt="mother-baby%20cuddle"></div>
+		
+	<div class="banner-photo">
+		<img src="img/photos/small/father_%26_baby_sunset.jpg" alt="father_%26_baby_sunset"></div>
+	
+</div>
+
+	<div class="quote"> "A person's a person, no matter how small."
+— Dr. Seuss</div>
+</div>
+
+
+<div class="container-noflex">
+
+<div class="blurb-noflex">
+	
+<!--page stuff goes here -->
+<h3>Testimonials</h3>
+<p>Dorset Parent Infant Partnership has come far from the early days of individual helpers. We now have a coordinated network of professionals working together and making great strides in the right direction. Below is the view of one parent on what can be achieved through parent infant therapy.</p>
+
+<p><i>“It has completely changed my life. I feel so happy to have Bella and Jacob, so we can be a family again. I couldn’t really see how bad I was, but Parent Infant Psychotherapy was the essential aid in my recovery and I totally love being a parent now. I think I would have lost Bella into care if it hadn’t been for this unique service” </i></p>
+<p>Liz</p>
+
+
+<br/><br/><br/>
+
+</div> <!-- end of blurb -->
+	
+
+</div> <!-- end of container -->
+<br><br>
+<footer>
+	<?php include 'includes/footer.php'; ?>
+</footer>
+
+</body>
+</html>
